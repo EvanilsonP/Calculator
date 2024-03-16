5f57cb454a8fa6e575f4a9823fdad02c1498e839
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-16 13:47:03 -0300

    Buttons

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,20 +8,59 @@ public class Calculator implements ActionListener {
     JTextField textField;
     JButton[] numberButtons = new JButton[10];            // number os buttons we need in a calculator
     JButton[] functionButtons = new JButton[8];           // number of function buttons we need in a calculator
-    JButton addbButton, subButton, multButton, divButton; 
+    JButton addButton, subButton, multButton, divButton; 
+    JButton decButton, equButton, delButton, clrButton, negButton;
     JPanel panel;
 
     Font myFont = new Font("ink Free", Font.BOLD, 30);
 
-    double numb1 = 0, numb2 = 0;
+    double numb1 = 0, numb2 = 0, result = 0;
     char operator;
 
     Calculator() {
-        Calculator calc = new Calculator();
+
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
+        frame.setLayout(null);
+
+        textField = new JTextField();
+        textField.setBounds(50, 25, 300, 50);
+        textField.setFont(myFont);
+        textField.setEditable(false);
+
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        multButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        clrButton = new JButton("Delete");
+        negButton = new JButton("Clear");
+
+        // Adding buttons to the array functionButtons
+		functionButtons[0] = addButton;
+		functionButtons[1] = subButton;
+		functionButtons[2] = multButton;
+		functionButtons[3] = divButton;
+		functionButtons[4] = decButton;
+		functionButtons[5] = equButton;
+		functionButtons[6] = delButton;
+		functionButtons[7] = clrButton;
+		functionButtons[8] = negButton;
+
+        for(int i = 0; i < 8; i++) {
+            functionButtons[i].addActionListener(this);
+            functionButtons[i].setFont(myFont);
+            functionButtons[i].setFocusable(false);
+        };
+
+        frame.add(textField);
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-
+        Calculator calc = new Calculator();
     }
 
     @Override
